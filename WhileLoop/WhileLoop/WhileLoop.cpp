2fab908ee406bb4a55cd1bc2d71a49492460ebfa
Abulqasim <abulqasim.nazarov@gmail.com>
2025-04-13 12:06:13 +0400

    If End

---
+++ WhileLoop/WhileLoop/WhileLoop.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    cout << "Enter num1:", cin >> num1;
+    cout << "Enter num2:", cin >> num2;
+    cout << "Enter num3:", cin >> num3;
+
+    if (num1 > 0)
+    {
+        cout << num1 << endl;
+    }
+    else if (num2 > 0)
+    {
+        cout << num2 << endl;
+
+    }
+     
+   else
+    {
+        cout << num3 << endl;
+    }
+    if (num2 > 0)
+    {
+        cout << num2 << endl;
+    }
+    else if (num1 > 0)
+    {
+        cout << num1 << endl;
+    }
+
+    else
+    {
+        cout << num3 << endl;
+    }
+    if (num3 > 0)
+    {
+        cout << num3 << endl;
+    }
+    else if (num2 > 0)
+    {
+        cout << num2 << endl;
+    }
+
+    else
+    {
+        cout << num1 << endl;
+    }
+        
+
+
+
+
+
+
+
+
+    /*if (num1 < num2 && num1 < num3)
+    {
+        cout << num1 << endl;
+    }
+    else if (num2 < num1 && num2 < num3)
+    {
+        cout << num2 << endl;
+    }
+    else {
+        cout << num3 << endl;
+    }
+
+    if (num1 == num2 && num1 == num3)
+    {
+        cout << num1 << endl;
+    }
+    else if (num2 == num3)
+    {
+        cout << num2 << endl;
+    }
+    else
+    {
+        cout << num3 << endl;
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*int num1 = 3;
+    int num2 = 3;
+
+
+    if (num1 < num2)
+    {
+        cout << "num1>num2" << endl;
+    }
+    else if (num1 == num2) {
+        cout << "num1=num2" << endl;
+    }
+    else {
+        cout << "num2>num1" << endl;
+    }*/
+
+    
+
+
+
+
+
+
+
+
+
+
+    /*char symbol = '%';
+    cout << int(symbol) << endl;*/
+
+
+
+
+
+
+
+    /*User:
+     Name
+     Surname
+     Age
+     IsAdult
+    */
+   
+    /*string Name = "Cavad";
+    string Surname = "Ramazanzade";
+    int Age = 14;
+    bool IsAdult = false;
+
+    cout << Name << endl;
+    cout << Surname << endl;
+    cout << Age << endl;
+    cout << IsAdult << endl;*/
+
+    
+}
+
