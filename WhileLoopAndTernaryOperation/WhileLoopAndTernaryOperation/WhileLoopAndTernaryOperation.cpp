9727f77962416e4e7ec5def0a62743e926eea68a
Abulqasim <abulqasim.nazarov@gmail.com>
2025-04-17 18:02:03 +0400

    While loop begin

---
+++ WhileLoopAndTernaryOperation/WhileLoopAndTernaryOperation/WhileLoopAndTernaryOperation.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	while (i < 10)
+	{
+		cout << "hello" << endl;
+		cout << "hello" << endl;
+		cout << "hello" << endl;
+		i = i + 1;
+		//i++;
+		//i+=1;
+	}
+
+	/*int num3 = 78;
+
+	cout << num3 << "Hello Cavad" << 56 << "Poka Cavad" << 90 << endl;*/
+
+
+	//User vvodit chislo
+	//7*1=7
+	//7*2=14
+	//7*3=21
+	//...
+	/*int i = 0;
+	int num1 = 0;
+	cout << "Enter num1: ", cin >> num1;
+
+	while (i<10)
+	{
+		cout << num1 << "*" << i << "=" << num1 * i << endl;
+		i++;
+	}*/
+
+
+
+	/*int num1, num2;
+	cout << "Enter num1: ", cin >> num1;
+	cout << "Enter num2: ", cin >> num2;
+
+	while (num1<num2)
+	{
+		if (num1 % 2 ==0)
+		{
+			cout << num1 << endl;
+		}
+		num1++;
+	}*/
+
+
+
+	/*int i = 0;
+	while (i < 5)
+	{
+		cout << "Hello" << endl;
+		i++;
+	}*/
+	
+
+
+
+
+
+	
+	
+
+
+
+
+
+	/*int num1 = 10;
+	int num2 = 3;
+
+	int test = (num1 > num2) ? num1 : num2;
+	cout << test << endl;*/
+
+
+	
+}
