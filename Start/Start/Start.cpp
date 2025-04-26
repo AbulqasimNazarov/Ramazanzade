857c49361c104d12f4d681dc94544919ed1062ee
Abulqasim <abulqasim.nazarov@gmail.com>
2025-04-26 11:34:50 +0400

    Start

---
+++ Start/Start/Start.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+    /*int num1 = -3;
+    int num2 = -5;
+
+
+    if (num1>0 && num2>0)
+    {
+        cout << num1 + num2 << endl;
+    }
+
+    if (num1 > 0 || num2 > 0)
+    {
+        cout << num1 + num2 << endl;
+    }
+
+
+    if (num1 > 0 and num2 > 0)
+    {
+        cout << num1 + num2 << endl;
+    }
+
+
+    if (num1 > 0 or num2 > 0)
+    {
+        cout << num1 + num2 << endl;
+    }*/
+
+
+
+
+
+
+    /*user vvodit chislo.Yesli chislo chetnoye to nayti 10% etovo chisla,
+    Yesli chislo nechetnoye i delitsa na 3 bez ostatka to nayti 30% etovo chisla
+    Yesli chislo nechetnoye i ne delitsa na 3 to nayti 50% etovo chisla*/
+
+
+
+
+    /*int test = 0;
+    cin >> test;*/
+
+
+
+
+
+
+
+
+    /*int num1 = 8;
+    int num2 = 8;
+
+    if (num1>num2)
+    {
+        cout << "More" << endl;
+    }
+    else if (num1 == num2) {
+
+    }
+    else if (num1 != num2) {
+
+    }
+    else if (num1 < num2) {
+
+    }
+    else 
+    {
+
+        cout << "Less" << endl;
+    }*/
+    
+
+
+    /*if (true)
+    {
+        
+        cout << num << endl;
+
+
+        if (true)
+        {
+            int num = 0;
+            cout << num << endl;
+
+            if (true)
+            {
+                cout << num << endl;
+            }
+        }
+
+    }*/
+
+
+    
+    /*char symbol = 'h';
+    string text1 = " Hello ";*/
+    
+   
+
+
+
+
+    ////Condition
+    //int num1 = 5;
+    //int num2 = 9;
+
+    ////Ispolzovat uslovniye OPERATORI
+    //bool test1 = num1 < num2;
+    //bool test2 = num1 > num2;
+    //bool test3 = num1 >= num2;
+    //bool test4 = num1 <= num2;
+    //bool test5 = num1 != num2;
+    //cout << test1 << endl;
+    //cout << test2 << endl;
+    //cout << test3 << endl;
+    //cout << test4 << endl;
+    //cout << test5 << endl;
+
+
+
+
+
+
+
+    //int num1 = 8;
+    //int num2 = 7;
+
+    //cout << num1 + num2 << endl; //+ eto arifmeticheskiy operator
+    //cout << num1 - num2 << endl;
+    //cout << num1 * num2 << endl;
+    //cout << num1 / num2 << endl;
+    //cout << num1 % num2 << endl;
+
+
+
+
+
+
+
+
+    //int number = 4523676827812988999; //initialization => create
+    //char symbol = 'a';
+    //double number2 = 9.7;
+    //string text = "Hello";
+    //long long number3 = 4523676827812988999;
+
+    //cout << number<< endl;
+
+
+
+
+
+    //cout << "Hello World!\n";
+    /* sahdavchjadvd
+    asvav
+    adfafv
+    ffvaeffv
+    affvaeefv
+    */
+}
+
+
+
+
+
