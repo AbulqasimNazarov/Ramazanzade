f77277957737cce13b354448499815350f70bd04
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-05-08 17:16:51 +0400

    While repeating

---
+++ WhileRepeat/WhileRepeat/WhileRepeat.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	/*string text = "Hello world";
+	int i = text.size() -1;
+	while (i >= 0)
+	{
+		cout << text[i] << endl;
+		i--;
+	}*/
+
+
+
+
+
+	/*string text = "Hello world";
+
+	cout << text[5] << endl;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*int i = 0;
+	while (i<1000)
+	{
+		if (i % 5 == 0 && i % 7 == 0)
+		{
+			cout << i << endl;
+		}
+		i++;
+	}*/
+	
+
+
+
+
+
+
+
+
+
+	/*int num1 = 0;
+	int i = 1;
+	cout << "Enter number :", cin >> num1;
+	while (i<100)
+	{
+		if (num1 % i == 0)
+		{
+			cout << i << endl;
+		}
+		i++;
+	}*/
+
+
+
+	/*int num1 = 0;
+	int result = 0;
+	cout << "Enter number :", cin >> num1;
+
+	while (true)
+	{
+		if (num1 != 0)
+		{
+			result += num1;
+			cout << "Enter number :", cin >> num1;
+		}
+		else {
+			break;
+		}
+		
+	}
+	cout << result << endl;*/
+
+
+
+
+
+	//int num = 0;
+	//int i = 0;
+	//cout << "Enter number: ", cin >> num; //8756    6578
+
+	//while (num>0) 
+	//{
+	//	int test = num % 10;
+	//	cout << test << endl;
+	//	num = num / 10;
+
+	//}
+
+
+
+
+
+	/*int i = 20;
+	while (i > 0)
+	{
+		cout << i << endl;
+		i--;
+	}*/
+
+	
+
+}
+
