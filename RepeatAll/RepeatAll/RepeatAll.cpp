99bdc5ea40bc99327d8bc13ed383e2399896e3e5
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-06-01 12:11:12 +0400

    Repeat continue

--- RepeatAll/RepeatAll/RepeatAll.cpp
+++ RepeatAll/RepeatAll/RepeatAll.cpp
@@ -4,8 +4,63 @@ using namespace std;
 
 int main()
 {
-    bool cap = 7 < 9;
-    cout << cap << endl;
+    int num1 = 935;
+    int num2 = num1 % 10;
+    if (true)
+    {
+
+    }
+   
+
+
+
+
+
+    //bool cap1 = true;
+    //bool cap2 = false;
+    //bool cap3 = 0;
+    //bool cap4 = 1;
+    //bool cap5 = 2345;
+    //bool cap6 = 5>3;
+    //bool cap7 = num1>num2;  // >, <, >=, <=, ==, !=
+    //bool cap8 = !cap1;
+
+    //if (cap6)
+    //{
+    //    cout << "True" << endl;
+    //    return 0;
+    //}
+
+    //cout << "Ne xcohu" << endl;
+
+
+
+
+    /*int num1 = -23890787;
+    unsigned int num9= 3745;
+    unsigned short num3 = 2345;
+    char symbol = 97;
+    cout << num3 << endl;
+    
+
+
+
+    float num2 = 45.6;
+    long long num4 = 89236751963754981;
+    long num5 = 23453245;
+    double num6 = 1234.7;*/
+
+
+
+
+
+
+
+
+
+
+    /*bool cap = 7 < 9;
+    cout << cap << endl;*/
 
     
     /*bool test1 = true;
