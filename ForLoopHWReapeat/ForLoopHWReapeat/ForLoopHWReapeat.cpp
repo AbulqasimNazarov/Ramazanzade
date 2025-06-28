10b615eea7424e3d834db1a4b26721c02c8ca1ad
Abulqasim <abulqasim.nazarov@gmail.com>
2025-06-28 13:05:24 +0400

    Functions start

---
+++ ForLoopHWReapeat/ForLoopHWReapeat/ForLoopHWReapeat.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+
+
+
+
+    /*int length = 0;
+    int heigth = 0;
+    cout << "Enter length: ", cin >> length;
+    cout << "Enter heigth: ", cin >> heigth;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < heigth; j++)
+        {
+            if (i == 0 || i == length-1 || j == 0 || j == heigth-1)
+            {
+                cout << "#";
+            }
+            else {
+                cout << j;
+            }
+        }
+        cout << endl;
+    }*/
+}
+
---
+++ TernaryOperationsContinue/TernaryOperationsContinue/TernaryOperationsContinue.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+    /*string text1 = "Hello";
+    string text2 = "HI";
+    bool cap = (text1.length() > text2.length()) ? false : true;
+    cout << cap << endl;
+
+    if (cap)
+    {
+        cout << text2.length() << endl;
+    }*/
+    
+
+    /*int num1 = 97;
+    int num2 = 111;
+    char result = (num1 < num2) ? num1 : num2;
+    cout << result << endl;*/
+
+
+    /*int num1 = 9;
+    int num2 = 11;
+    int result = (num1 < num2) ? num1 : num2;
+    cout << result << endl;*/
+}
+
