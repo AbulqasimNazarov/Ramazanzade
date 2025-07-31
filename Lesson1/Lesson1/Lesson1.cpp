6fe6fb0a4df348ee66777c1b61d369c02e1ed471
Abulqasim <abulqasim.nazarov@gmail.com>
2025-07-31 13:47:00 +0400

    Lesson 1 types

---
+++ Lesson1/Lesson1/Lesson1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 90;
+    int num2 = 34;
+    bool test1 = num1 > num2;
+
+
+
+
+    /*if (test1 != true)
+    {
+        cout << "TEST" << endl;
+    }*/
+
+
+    /*if (test1==false)
+    {
+        cout << "TEST" << endl;
+    }*/
+
+
+    /*if (!test1)
+    {
+        cout << "TEST" << endl;
+        
+    }*/
+    
+    /*if (test1)
+    {
+        cout << "TEST" << endl;
+        
+    }*/
+
+
+
+
+
+
+
+
+    /*bool test2 = num1 >= num2;
+    bool test3 = num1 <= num2;
+    bool test4 = num1 < num2;
+    bool test5 = num1 == num2;
+    bool test6 = num1 != num2;*/
+
+    /*cout << test2 << endl;
+    cout << test3 << endl;
+    cout << test4 << endl;
+    cout << test5 << endl;
+    cout << test6 << endl;*/
+
+
+    //int result = num1 + num2;
+    /*char symbol1 = 'a';
+    char symbol2 = 98;
+
+    cout << char(symbol1) + char(symbol2) << endl;*/
+  
+    
+    /*char symbol = -45;
+    cout << symbol << endl;*/
+
+
+    /*
+    int num1 = 45;
+    short num2 = 44;
+    string text = "";
+    while (true)
+    {
+        cin >> text;
+        cout << text << endl;
+
+    }*/
+
+}
+
